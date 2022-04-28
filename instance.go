4a344307742d1b7cc24b76ae080048d8defc520e
yatniel <yatniel@qq.com>
2022-04-29 00:35:39 +0800

    meta

--- instance.go
+++ instance.go
@@ -7,10 +7,6 @@ import (
 	"github.com/chefsgo/chef"
 )
 
-func (this *Instance) newContext() *Context {
-	return &Context{inst: this}
-}
-
 // 收到消息
 // 待优化，加入协程池，限制单个事件的并发
 func (this *Instance) Serve(alias string, data []byte) {
@@ -18,24 +14,20 @@ func (this *Instance) Serve(alias string, data []byte) {
 		alias = strings.TrimPrefix(alias, this.config.Prefix)
 	}
 
-	ctx := this.newContext()
+	ctx := &Context{inst: this}
 
-	// 解析过来的数据
-	meta := chef.Meta{}
-	err := chef.Unmarshal(this.config.Codec, data, &meta)
-	if err == nil {
-		ctx.Context = chef.NewContext(meta)
-	} else {
-		ctx.Context = chef.NewContext()
-	}
-
-	//名称和别名
-	ctx.Alias = alias
 	relate := this.module.relateKey(this.name, alias)
 	if name, ok := this.module.relates[relate]; ok {
 		ctx.Name = name
 		if cfg, ok := this.module.events[ctx.Name]; ok {
 			ctx.Config = &cfg
+		}
+
+		// 解析元数据
+		metameta := chef.Metadata{}
+		err := chef.Unmarshal(this.config.Codec, data, &metameta)
+		if err == nil {
+			ctx.Metadata(metameta)
 		}
 	}
 
@@ -79,7 +71,7 @@ func (this *Instance) authorizing(ctx *Context) {
 func (this *Instance) arguing(ctx *Context) {
 	if ctx.Config.Args != nil {
 		argsValue := Map{}
-		res := chef.Mapping(ctx.Config.Args, ctx.Value, argsValue, ctx.Config.Nullable, false, ctx.Context)
+		res := chef.Mapping(ctx.Config.Args, ctx.Value, argsValue, ctx.Config.Nullable, false, ctx.Timezone())
 		if res != nil && res.Fail() {
 			ctx.Failed(res)
 		}
